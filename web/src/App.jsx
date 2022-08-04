f4f80677ee4fbf174b347ea20d0d1a92054f1350
Insung <insungmul@live.co.uk>
2022-08-04 11:11:57 +0100

    first commit

---
+++ web/src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles/index.scss";
+
+function App() {
+  return (
+    <main>
+      <img className="logo u-margin-bottom" src="/sky-logo.png" alt="Sky logo" />
+      <p className="c-text-lead">Let's Get Started.</p>
+    </main>
+  );
+}
+
+export default App;
